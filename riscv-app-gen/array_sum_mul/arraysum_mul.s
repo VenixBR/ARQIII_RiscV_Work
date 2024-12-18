c6b954b036065050431c037af3ebbbf43a788525
João Pedro Buzatti Mendes <joao.buzattimendes@outlook.com>
2024-12-18 17:52:40 -0300

    Adding bin and elf for new programs

---
+++ riscv-app-gen/array_sum_mul/arraysum_mul.s
@@ -0,0 +1,9 @@
+
+arraysum_mul.elf:     file format elf32-littleriscv
+
+
+Disassembly of section .text.startup:
+
+80000000 <main>:
+80000000:	00000513          	li	a0,0
+80000004:	00008067          	ret
