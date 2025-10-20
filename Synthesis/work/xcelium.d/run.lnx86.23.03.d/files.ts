3830385eea607554eb3df189728036224a003957
VenixBR <viniciusschultz2405@gmail.com>
2025-10-20 18:32:28 -0300

    synthesis paths and directories ready

---
+++ Synthesis/work/xcelium.d/run.lnx86.23.03.d/files.ts
@@ -0,0 +1,5 @@
+1759264641 /home/schultz/Documents/ARQIII_Works/ACTV_05_Sort/HDL/Comparator.v
+1760561815 /home/schultz/Documents/ARQIII_Works/ACTV_05_Sort/HDL/ControlPath.v
+1760562056 /home/schultz/Documents/ARQIII_Works/ACTV_05_Sort/HDL/DataPath.v
+1760561840 /home/schultz/Documents/ARQIII_Works/ACTV_05_Sort/HDL/BubbleSort.v
+1760562166 /home/schultz/Documents/ARQIII_Works/ACTV_05_Sort/HDL/BubbleSort_tb.sv
